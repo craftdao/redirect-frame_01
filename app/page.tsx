c8671135cba37d66be18d2be99dcd24511ff07b7
lukaschmiell <info@lukaschmiel.com>
2024-01-30 23:56:16 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,19 @@ import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
-      {label: 'Click to unlock', action: 'post_redirect'}
+      {label: 'Subscribe', action: 'post_redirect'}
+      {label: 'Subscribe', action: 'post_redirect'}
   ],
   image: 'https://redirect-frame-01-craftdao.vercel.app/video.png',
   post_url: 'https://redirect-frame-01-craftdao.app/api/frame',       
 });
 
 export const metadata: Metadata = {
-  title: 'Redirect frame',
+  title: 'STVN Frame',
   description: 'LFG',
   openGraph: {
-    title: 'Redirect frame',
-    description: 'LFG',
+    title: 'STVN',
+    description: 'Trailer',
     images: ['https://redirect-frame-01-craftdao.vercel.app/video.png'],
   },
   other: {
